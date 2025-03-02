4caa0a859f6fcfc8697b064a5157888754730a60
Sandy-8-bit <santhosh.vp.aiml.2022@snsct.org>
2025-03-02 14:12:03 +0530

    Admin action and resposnive

--- obj/Debug/net8.0/Blog Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Blog Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aade7673be5cada3fc21e1287f3d78cb790e460")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f06ab270c179ebafd3b4086133d53d1d2315352")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
